4a63136a4296994db337a90c04e0f743cb548da1
Swadhin Nanda <nanda.swadhin@gmail.com>
2021-06-29 18:49:52 +0200

    added black and formatted the query

--- scripts/utils/etherscan_utils/contract_interactions.py
+++ scripts/utils/etherscan_utils/contract_interactions.py
@@ -1,1 +1,12 @@
-https://api.etherscan.io/api?module=account&action=txlist&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae&startblock=0&endblock=99999999&sort=asc&apikey=$ETHERSCAN_API_KEY
+import os
+
+query = f"""
+https://api.etherscan.io/api?
+module=account
+&action=txlist
+&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
+&startblock=0
+&endblock=99999999
+&sort=asc
+&apikey={os.environ["ETHERSCAN_API_KEY"]}
+"""
