892498e1535e0de87db032fefdc287afab907333
Swadhin Nanda <11488427+bout3fiddy@users.noreply.github.com>
2021-07-01 00:56:07 +0200

    added code to get tokens transferred for tricrypto contract

--- scripts/utils/tricrypto_utils.py
+++ scripts/utils/tricrypto_utils.py
@@ -1,16 +1,44 @@
+import os
+
 from etherscan.proxies import Proxies
+from etherscan.contracts import Contract
+from web3 import Web3
+
+WEB3 = Web3(Web3.HTTPProvider("http://fullnode.dappnode:8545"))
+TRICRYPTO_CONTRACT_ADDRESS = "0x331aF2E331bd619DefAa5DAc6c038f53FCF9F785"
 
 
-def get_erc20_token_transfers(tx_hash: str, tx_fetcher: Proxies):
+def get_erc20_token_into_contract(tx: dict, tx_fetcher: Proxies):
 
-    tx_receipt = tx_fetcher.get_transaction_receipt(tx_hash=tx_hash)
+    tricrypto_contract = Contract(
+        address=TRICRYPTO_CONTRACT_ADDRESS, api_key=os.environ["ETHERSCAN_API_KEY"]
+    )
+    tricrypto_contract_abi = tricrypto_contract.get_abi()
+    tricrypto_contract_web3 = WEB3.eth.contract(
+        address=TRICRYPTO_CONTRACT_ADDRESS, abi=tricrypto_contract_abi
+    )
+    tx_input = tx["input"]
+    decoded_input = tricrypto_contract_web3.decode_function_input(tx_input)
+    method_name = decoded_input[0].__dict__["abi"]["name"]
 
-    return tx_receipt
+    # only continue if method called was add_liquidity in the contract
+    if method_name != "add_liquidity":
+        return {}
+    tx_amounts = decoded_input[1]["_amounts"]
+    input_tokens = {
+        "liquidity_added": {
+            "USDT": tx_amounts[0],
+            "WBTC": tx_amounts[1],
+            "ETH": tx_amounts[2],
+        }
+    }
+    return input_tokens
 
 
 def main():
-    from .etherscan_utils.contract_interactions import Contract
+    from etherscan_utils.contract_interactions import Contract
     import os
+    import json
 
     tx_fetcher = Proxies(api_key=os.environ["ETHERSCAN_API_KEY"])
 
@@ -22,7 +50,9 @@ def main():
 
     user_txes = tricrypto_contract.get_tx_with(addr=user.lower())
     for tx in user_txes:
-        tokens_added = get_erc20_token_transfers(tx["hash"], tx_fetcher=tx_fetcher)
+        print("Block Number: ", tx["blockNumber"])
+        tokens_added = get_erc20_token_into_contract(tx, tx_fetcher=tx_fetcher)
+        print(json.dumps(tokens_added, indent=4))
 
 
 if __name__ == "__main__":
