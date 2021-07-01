43a20a15181230907a2542fa55b490f9b89676ba
Swadhin Nanda <11488427+bout3fiddy@users.noreply.github.com>
2021-07-01 19:40:44 +0200

    folder organisation

--- scripts/utils/tricrypto_utils/transaction_parser.py
+++ scripts/utils/tricrypto_utils/transaction_parser.py
@@ -1,6 +1,5 @@
 import os
 
-from etherscan.proxies import Proxies
 from etherscan.contracts import Contract
 from web3 import Web3
 
@@ -8,7 +7,7 @@ WEB3 = Web3(Web3.HTTPProvider("http://fullnode.dappnode:8545"))
 TRICRYPTO_CONTRACT_ADDRESS = "0x331aF2E331bd619DefAa5DAc6c038f53FCF9F785"
 
 
-def get_erc20_token_into_contract(tx: dict, tx_fetcher: Proxies):
+def get_added_liquidity(tx: dict):
 
     tricrypto_contract = Contract(
         address=TRICRYPTO_CONTRACT_ADDRESS, api_key=os.environ["ETHERSCAN_API_KEY"]
@@ -27,9 +26,9 @@ def get_erc20_token_into_contract(tx: dict, tx_fetcher: Proxies):
     tx_amounts = decoded_input[1]["_amounts"]
     input_tokens = {
         "liquidity_added": {
-            "USDT": tx_amounts[0],
-            "WBTC": tx_amounts[1],
-            "ETH": tx_amounts[2],
+            "USDT": tx_amounts[0] / 10 ** 7,
+            "WBTC": tx_amounts[1] / 10 ** 8,
+            "ETH": tx_amounts[2] / 10 ** 18,
         }
     }
     return input_tokens
@@ -40,18 +39,16 @@ def main():
     import os
     import json
 
-    tx_fetcher = Proxies(api_key=os.environ["ETHERSCAN_API_KEY"])
-
     tricrypto_contract = Contract(
         address="0x331aF2E331bd619DefAa5DAc6c038f53FCF9F785",
         api_key=os.environ["ETHERSCAN_API_KEY"],
     )
-    user = "0xcAf2d3f6c4A375ccEC74eB7eD5c03f5B6cd8876E"
+    user = "0x0716266cc5d3c443e30b0c4e9c72afa33778e1de"
 
     user_txes = tricrypto_contract.get_tx_with(addr=user.lower())
     for tx in user_txes:
         print("Block Number: ", tx["blockNumber"])
-        tokens_added = get_erc20_token_into_contract(tx, tx_fetcher=tx_fetcher)
+        tokens_added = get_added_liquidity(tx, tx_fetcher=tx_fetcher)
         print(json.dumps(tokens_added, indent=4))
 
 
