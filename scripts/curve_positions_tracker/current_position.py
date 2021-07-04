c5b01b305af79c90b99cab03225013a52e55fc73
Swadhin Nanda <11488427+bout3fiddy@users.noreply.github.com>
2021-07-04 19:54:37 +0200

    v0.0.1 is ready

---
+++ scripts/curve_positions_tracker/current_position.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+
+from web3 import Web3
+
+from scripts.curve_positions_tracker.get_balances import *
+from scripts.curve_positions_tracker.utils import get_prices_of_coins, init_contract
+from scripts.curve_positions_tracker.tricryptopool_methods import *
+from scripts.curve_positions_tracker.constants import TRICRYPTO_CONTRACT_ADDR
+
+tricrypto_contract = init_contract(TRICRYPTO_CONTRACT_ADDR)
+
+
+def get_tricrypto_liquidity_positions(user_addr: str):
+
+    user_addr = Web3.toChecksumAddress(user_addr)
+
+    # get total number of LP tokens a user has in the tricrypto pool:
+    gauge_bal_convex = get_convex_gauge_bal(user_addr)
+    gauge_bal_curve = get_curve_gauge_bal(user_addr)
+    gauge_bal = gauge_bal_convex + gauge_bal_curve
+    lp_token_bal = get_lp_token_bal(user_addr)
+    token_balance_to_calc_on = gauge_bal + lp_token_bal
+
+    time_now = datetime.utcnow().strftime("%Y%m%dT%H%M%S")
+
+    # we calculate position on the following token balance (tokens in gauge + free lp tokens)
+    if not token_balance_to_calc_on:
+        return {time_now: {}}
+
+    token_prices_coingecko = get_prices_of_coins()
+
+    # calculating position of coins in LP:
+    # TODO: currently the following is hardcoded to tricrypto. make it more flexible.
+    # this will ensure that the code is prepared for other curve v2
+    token_names = ["USDT", "WBTC", "ETH"]
+    final_positions = {}
+    for i in range(len(token_names)):
+
+        _token = init_contract(token_contract(i, tricrypto_contract=tricrypto_contract))
+        _token_decimals = _token.functions.decimals().call()
+        _price = 1
+        price_from_curve_oracle = _price
+        if token_names[i] not in ["USDT"]:
+            price_from_curve_oracle = price_oracle(
+                token_index=i - 1, tricrypto_contract=tricrypto_contract
+            )
+            _price = price_from_curve_oracle / 10 ** 18  # 18 digit precision
+
+        _coins = (
+            calc_withdraw_one_coin(
+                token_balance_to_calc_on, i, tricrypto_contract=tricrypto_contract
+            )
+            / 10 ** _token_decimals
+        )
+        _val = _coins * _price
+        position_data = {
+            "token_contract_address": _token.address,
+            "curve_oracle_price_usd": _price,
+            "num_tokens": _coins,
+            "value_tokens_usd": _val,
+            "coingecko_price_usd": token_prices_coingecko[token_names[i]],
+        }
+        final_positions[token_names[i]] = position_data
+
+    timestamped_output = {time_now: final_positions}
+
+    return timestamped_output
---
+++ tricrypto_tracker.py
@@ -0,0 +1,75 @@
+import argparse
+import sys
+
+from scripts.curve_positions_tracker.liquidity_txes import get_added_liquidity
+from scripts.curve_positions_tracker.current_position import (
+    get_tricrypto_liquidity_positions,
+)
+
+# ---- CLI ----
+# The functions defined in this section are wrappers around the main Python
+# API allowing them to be called directly from the terminal as a CLI
+# executable/script.
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="Get TriCrypto positions and deposit info for address."
+    )
+    parser.add_argument(
+        dest="address",
+        help="Address to fetch info for.",
+        type=str,
+    )
+    return parser.parse_args(args)
+
+
+def main(args):
+    """Wrapper allowing :funcs:`added_liquidity` and `get_tricrypto_liquidity_positions`
+    to be called with string arguments in a CLI fashion.
+
+    It prints the result to the ``stdout`` in a nicely formatted message.
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["0x331aF2E331bd619DefAa5DAc6c038f53FCF9F785"]``).
+    """
+    import json
+
+    args = parse_args(args)
+    print(f"User Address: {args.address}")
+    print("Fetching all deposits to Curve v2 TriCrypto pool.")
+    added_liquidity = get_added_liquidity(address=args.address)
+    print(json.dumps(added_liquidity, indent=4))
+    lp_positions = get_tricrypto_liquidity_positions(user_addr=args.address)
+    print(json.dumps(lp_positions, indent=4))
+
+
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    # ^  This is a guard statement that will prevent the following code from
+    #    being executed in the case someone imports this file instead of
+    #    executing it as a script.
+    #    https://docs.python.org/3/library/__main__.html
+
+    # After installing your project with pip, users can also run your Python
+    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
+    #
+    #     python -m ${qual_pkg}.skeleton 42
+    #
+    run()
