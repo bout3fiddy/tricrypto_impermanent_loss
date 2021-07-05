d7aa8b85dc40bec752db17f4cb31fa23fffc5a35
Swadhin Nanda <11488427+bout3fiddy@users.noreply.github.com>
2021-07-05 23:09:51 +0200

    added methods to fetch current position

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 
 from projects.api.route.liquidity_txes import get_added_liquidity
+from projects.api.route.current_position import get_tricrypto_liquidity_positions
+
 
 app = Flask(__name__)
 
@@ -10,11 +12,16 @@ def home():
     return jsonify({"about": "TriCrypto Curve Tracker"})
 
 
-# adding variables
-@app.route("/user/<user_address>")
-def post_added_liquidity(user_address):
+@app.route("/user/historical_added_liquidity/<user_address>")
+def fetch_added_liquidity(user_address):
     added_liquidity = get_added_liquidity(address=user_address)
     return jsonify(added_liquidity)
+
+
+@app.route("/user/current_lp_values/<user_address>")
+def calculate_current_positions(user_address):
+    current_position = get_tricrypto_liquidity_positions(user_addr=user_address)
+    return jsonify(current_position)
 
 
 if __name__ == "__main__":
